a93480b087487856f38ad83d015a2364992d30f0
navrisham <31776154+navrisham1@users.noreply.github.com>
2017-11-21 14:26:22 -0500

    Add files via upload

--- DBConnection.java
+++ DBConnection.java
@@ -14,16 +14,18 @@ import java.sql.DriverManager;
  */
 public class DBConnection {
     static Connection conn = null;
-    public static Connection connect() {
+     public static Connection connect() {
         try {
+            
             Class.forName("com.mysql.jdbc.Driver");
             String url = "jdbc:mysql://127.0.0.1:3306/rideshare";
             conn = DriverManager.getConnection(url, "root", "root");
-            // change
-            return conn;
         } catch (Exception e) {
             System.out.println(e);
         }
-        return null;
+        finally{
+        
+        return conn;
+        }
     }
 }
