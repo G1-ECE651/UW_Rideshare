e54ae3ed528333ff90a291fc37f11ad13c9c0537
navrisham <31776154+navrisham1@users.noreply.github.com>
2017-11-23 04:35:00 -0500

    DATABASE

---
+++ Dump20171123.sql
@@ -0,0 +1,98 @@
+-- MySQL dump 10.13  Distrib 5.5.9, for Win32 (x86)
+--
+-- Host: 127.0.0.1    Database: rideshare
+-- ------------------------------------------------------
+-- Server version	5.5.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `rideshare`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `rideshare` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `rideshare`;
+
+--
+-- Table structure for table `drivermaster`
+--
+
+DROP TABLE IF EXISTS `drivermaster`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `drivermaster` (
+  `userid` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(45) DEFAULT NULL,
+  `name` varchar(60) DEFAULT NULL,
+  `contact` varchar(15) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  `status` tinyint(1) DEFAULT NULL,
+  `verified` tinyint(1) DEFAULT NULL,
+  `signup` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`userid`),
+  UNIQUE KEY `email_UNIQUE` (`email`),
+  UNIQUE KEY `contact_UNIQUE` (`contact`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `drivermaster`
+--
+
+LOCK TABLES `drivermaster` WRITE;
+/*!40000 ALTER TABLE `drivermaster` DISABLE KEYS */;
+/*!40000 ALTER TABLE `drivermaster` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `rides`
+--
+
+DROP TABLE IF EXISTS `rides`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `rides` (
+  `rideid` int(11) NOT NULL AUTO_INCREMENT,
+  `source` varchar(45) DEFAULT NULL,
+  `destination` varchar(45) DEFAULT NULL,
+  `path` varchar(200) DEFAULT NULL,
+  `status` tinyint(1) DEFAULT NULL,
+  `datetime` datetime DEFAULT NULL,
+  `userid` int(11) DEFAULT NULL,
+  `seats` int(11) DEFAULT NULL,
+  `cost` decimal(14,2) DEFAULT NULL,
+  `carname` varchar(75) DEFAULT NULL,
+  PRIMARY KEY (`rideid`),
+  KEY `userid` (`userid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rides`
+--
+
+LOCK TABLES `rides` WRITE;
+/*!40000 ALTER TABLE `rides` DISABLE KEYS */;
+/*!40000 ALTER TABLE `rides` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-11-23  4:32:33
